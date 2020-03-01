9c4f2367e6d7e2203a5c5e17a0b68e84c939687a
Sabina <sabina.bialic@gmail.com>
2020-02-29 19:55:07 -0500

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/layouts/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Sabina Bialic</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Sections = () => {
+  const anchors = ['About', 'Experience', 'Projects'];
+  //const sectionsArray = [<MainPage />, <Experience />, <Projects />];
+  //return (anchors.map((item, index) => { return (<ScrollableAnchor id={item} key={index}><div>{sectionsArray[index]}</div></ScrollableAnchor>) }));
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Header.css';
+import resume from "../../assets/files/Updated January 2020.pdf";
+
+var MobileMenuStyle = {
+    height: "0px"
+}
+const Anchors = () => {
+    const anchorsArr = ["Projects", "Experience", "About"];
+    return (anchorsArr.map(item => { return (<a key={item} href={`#${item}`}>{item}</a>) }));
+}
+
+export default class Header extends Component {
+  render() {
+    return (
+        <div className="nav-bar">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="logo slide-in-blurred-top">
+                            <span className="full-name">
+                                <h1>Sabina Bialic</h1>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="col flex slide-in-blurred-right">
+                        <div className="full-options">
+                            <a href={resume}>
+                                <div className="resume-link">
+                                    View Resume
+                                </div>
+                            </a>
+                            <Anchors />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
