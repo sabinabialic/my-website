537f798bb919a24678934b57e4d161ab22f91410
Sabina <sabina.bialic@gmail.com>
2020-08-14 23:30:28 -0400

    Updates

---
+++ precache-manifest.70f6185c476e9ada0edfad642b467658.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15171f2b30bee91b1b8c865e20ad3eeb",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "10c0e6ea68ee3b1f7c68",
+    "url": "/my-website/static/css/main.d6803c23.chunk.css"
+  },
+  {
+    "revision": "b9e62be12b849d14fa53",
+    "url": "/my-website/static/js/2.cb5a2857.chunk.js"
+  },
+  {
+    "revision": "16645896408a141b91c4e8727c0ec2d5",
+    "url": "/my-website/static/js/2.cb5a2857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10c0e6ea68ee3b1f7c68",
+    "url": "/my-website/static/js/main.ed5b1eb7.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/my-website/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/my-website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f49f3d2d9df5013c9bfaab7e3d39ee57",
+    "url": "/my-website/static/media/Raleway-Bold.f49f3d2d.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/my-website/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/my-website/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/my-website/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "71b656cbe85d7dc46340d74646de1215",
+    "url": "/my-website/static/media/Updated January 2020.71b656cb.pdf"
+  },
+  {
+    "revision": "b5e02104d5bd89ee225ed6defd500b1b",
+    "url": "/my-website/static/media/dp.b5e02104.jpg"
+  }
+]);
