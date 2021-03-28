ea3e99d5fea38815e597a2ed9e875dba29640cf5
Sabina <sabinabialic@cmail.carleton.ca>
2021-03-28 19:32:56 -0400

    Updating Projects Section

--- src/components/layouts/Projects.js
+++ src/components/layouts/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Projects.css';
-import hiitme from '../../assets/images/hiitMeMainScreen.png';
+import hiitme from '../../assets/images/FlutterIntervalTimerApp.png';
 
 export default class Projects extends Component {
   render() {
@@ -20,7 +20,7 @@ export default class Projects extends Component {
 
                       <div className="col2">
                         <div className="project-type">Mobile App</div>
-                        <div className="project-name">HiitMe Interval Timer</div>
+                        <div className="project-name">HIIT Interval Timer</div>
                         <div className="content">
                             <div className="content1"><hr /></div>
                             <div className="content2">
@@ -28,9 +28,13 @@ export default class Projects extends Component {
                                     <b>HiitMe</b> is an interval timer mobile app for iOS
                                     and Android, created using <b>Flutter</b> and <b>Dart</b>.
                                 </p>
-                                <a class="git-button" href="https://github.com/sabinabialic/interval_timer">
-                                	<span class="git-button-text">Github</span>
-                                	<span class="git-button-icon"><i class="fab fa-github" aria-hidden="true"></i></span>
+                                <a class="action-button" href="https://github.com/sabinabialic/interval_timer">
+                                	<span class="action-button-text">Github</span>
+                                	<span class="action-button-icon"><i class="fab fa-github" aria-hidden="true"></i></span>
+                                </a>
+                                <a class="action-button" href="https://www.youtube.com/watch?v=MMAfnOEFn6I">
+                                	<span class="action-button-text">App Demo</span>
+                                	<span class="action-button-icon"><i class="fab fa-youtube" aria-hidden="true"></i></span>
                                 </a>
                             </div>
                         </div>
